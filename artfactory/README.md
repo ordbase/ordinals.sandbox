5e5afa0a4d6e21f5c109bc6346d6c1766675d4ce
Gerald Bauer <gerald.bauer@gmail.com>
2022-10-06 20:01:06 +0200

    up

--- artfactory/README.md
+++ artfactory/README.md
@@ -99,17 +99,17 @@ shiba2 =  Artfactory.use( sheet )
 # step 2 - generate images via text (prompts)
 
 doge = shiba2.generate( 'Classic', '3D Glasses' )
-doge.save( './doge1-ii.png' )
-doge.zoom(4).save( './doge1-ii@4x.png' )
+doge.save( './doge1.png' )
+doge.zoom(4).save( './doge1@4x.png' )
 
 
 doge = shiba2.generate( 'Alien', 'Headband' )
-doge.save( './doge2-ii.png' )
-doge.zoom(4).save( './doge2-ii@4x.png' )
+doge.save( './doge2.png' )
+doge.zoom(4).save( './doge2@4x.png' )
 
 doge = doge.background( 'Matrix 1', 'Rainbow 1' )
-doge.save( './doge2b-ii.png' )
-doge.zoom(4).save( './doge2b-ii@4x.png' )
+doge.save( './doge2b.png' )
+doge.zoom(4).save( './doge2b@4x.png' )
 ```
 
 
--- artfactory/sandbox/test_generate.rb
+++ artfactory/sandbox/test_generate.rb
@@ -40,17 +40,17 @@ sheet = Pixelart::Spritesheet.read( './sandbox/config/spritesheet.png',
 shiba2 =  Artfactory.use( sheet )
 
 doge = shiba2.generate( 'Classic', '3D Glasses' )
-doge.save( './tmp/doge1.png' )
-doge.zoom(4).save( './tmp/doge1@4x.png' )
+doge.save( './tmp/doge1-ii.png' )
+doge.zoom(4).save( './tmp/doge1-ii@4x.png' )
 
 
 doge = shiba2.generate( 'Alien', 'Headband' )
-doge.save( './tmp/doge2.png' )
-doge.zoom(4).save( './tmp/doge2@4x.png' )
+doge.save( './tmp/doge2-ii.png' )
+doge.zoom(4).save( './tmp/doge2-ii@4x.png' )
 
 doge = doge.background( 'Matrix 1', 'Rainbow 1' )
-doge.save( './tmp/doge2b.png' )
-doge.zoom(4).save( './tmp/doge2b@4x.png' )
+doge.save( './tmp/doge2b-ii.png' )
+doge.zoom(4).save( './tmp/doge2b-ii@4x.png' )
 
 
 puts "bye"
