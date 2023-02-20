15a493508ed739fc73c4b7a352f813515eb7fa31
Gerald Bauer <gerald.bauer@gmail.com>
2023-02-20 19:15:44 +0100

    up

--- sandbox/ordinalpunks.rb
+++ sandbox/ordinalpunks.rb
@@ -1,7 +1,7 @@
 require 'cocos'
 
 
-data = read_json( "./tmp/ordinalpunks.json" )
+data = read_json( "./tmp/ordinalpunks.patch.json" )
 pp data
 
 
@@ -26,6 +26,41 @@ items.each_with_index do |rec,i|
   puts "#{i+1},  #{type},  #{accessories.count},  #{accessories.join(' / ')}"
 
 end
+
+
+## get attribute counts my categories
+
+stats = { 'Type' => { 'm' => Hash.new(0),
+                      'f' => Hash.new(0)}}
+
+
+items.each_with_index do |rec,i|
+
+  ## note: assume first entry is Type
+  type = rec['attributes'][0]['value']
+
+  gender = type.downcase.index( 'female' ) ?  'f' : 'm'
+
+
+  rec['attributes'].each do |h|
+      key   = h['trait_type']
+      value = h['value']
+
+      if key == 'Type'
+        stat = stats[ key ][ gender ]
+        stat[ value ]  += 1
+      else
+        category = stats[ key ] ||= {}
+        stat     = category[ value ] ||= { 'm' => 0,
+                                           'f' => 0 }
+        stat[ gender ] += 1
+      end
+  end
+end
+
+pp stats
+
+
 
 
 puts "bye"
